55fd8c336fe6c2a32bfba059f73b0e683f77c18f
Mathis Flüchter <mat.fluechter@web.de>
2018-02-25 17:38:02 +0100

    Add project files.

---
+++ neo-sql-db/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace neo_sql_db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
